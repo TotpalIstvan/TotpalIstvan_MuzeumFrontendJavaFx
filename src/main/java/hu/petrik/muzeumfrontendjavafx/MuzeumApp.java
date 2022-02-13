05371bbd499aea77156de5eec96dd0e2d732e652
TotpalIstvan <totpal.istvan@petrik.hu>
2022-02-13 15:28:43 +0100

    új viewok és classok, és átnevezés

--- src/main/java/hu/petrik/muzeumfrontendjavafx/MuzeumApp.java
+++ src/main/java/hu/petrik/muzeumfrontendjavafx/MuzeumApp.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class MuzeumApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MuzeumApp.class.getResource("statue-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MuzeumApp.class.getResource("megjelenit-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Szobrok és festmények");
         stage.setScene(scene);
---
+++ src/main/java/hu/petrik/muzeumfrontendjavafx/SzobormodositController.java
@@ -0,0 +1,4 @@
+package hu.petrik.muzeumfrontendjavafx;
+
+public class SzobormodositController {
+}
